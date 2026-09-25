95377fa6d755521332809a6fb9f982c23caa95e4
Dominic Oyugi <dominicoyugi1@gmail.com>
2026-09-25 12:16:50 +0300

    Update string reverse solution

--- 05-reverse-string.js
+++ 05-reverse-string.js
@@ -1,3 +1,7 @@
+///Problem 5: Reverse a string without using the built-in .reverse() method.
+
+///Example: reverseString("hello") → "olleh"
+//hint:use a loop
 function reverseString(str) {
   let reversed = "";
   for (let i = str.length - 1; i >= 0; i--) {
